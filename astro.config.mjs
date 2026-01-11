d3bb49fbbce9e9081574cb79b7b222aa59f23c5a
jinsyaoommen <jinsyaoommen@gmail.com>
2026-01-10 22:22:22 -0800

    Add custom domain thegoodoommen.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jinsyaoommen.github.io',
+  site: 'https://thegoodoommen.com',
   integrations: [react()]
 });
